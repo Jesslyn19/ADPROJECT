0c60a327991d4dcc86c4b5c99b076f1418d16102
Bernice Lim Jing Xuan <bernicelimjingxuan@gmail.com>
2025-05-28 22:16:36 +0800

    update sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -83,19 +83,17 @@ export default function Sidebar(props) {
   );
   var brand = (
     <div className={classes.logo}>
-      <a
-        href="https://www.creative-tim.com?ref=mdr-sidebar"
-        className={classNames(classes.logoLink, {
-          [classes.logoLinkRTL]: props.rtlActive,
-        })}
-        target="_blank"
-      >
-        <div className={classes.logoImage}>
-          <img src={logo} alt="logo" className={classes.img} />
-        </div>
-        {logoText}
-      </a>
+  <div
+    className={classNames(classes.logoLink, {
+      [classes.logoLinkRTL]: props.rtlActive,
+    })}
+  >
+    <div className={classes.logoImage}>
+      <img src={logo} alt="logo" className={classes.img} />
     </div>
+    Ultra Kutip
+  </div>
+  </div>
   );
   return (
     <div>
