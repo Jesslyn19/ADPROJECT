4a5f823b8cc7899001d6af1b84f591d1b2564017
Bernice Lim Jing Xuan <bernicelimjingxuan@gmail.com>
2025-05-30 23:15:53 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -19,11 +19,9 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import BubbleChart from "@material-ui/icons/BubbleChart";
+//import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
-import Notifications from "@material-ui/icons/Notifications";
-import Unarchive from "@material-ui/icons/Unarchive";
-import Language from "@material-ui/icons/Language";
+//import Notifications from "@material-ui/icons/Notifications";
 import ImageIcon from "@material-ui/icons/Image";
 import Image from "views/Image/Image.js";
 // core components/views for Admin layout
@@ -31,58 +29,49 @@ import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
-import Icons from "views/Icons/Icons.js";
+//import Icons from "views/Icons/Icons.js";
 import Maps from "views/Maps/Maps.js";
-import NotificationsPage from "views/Notifications/Notifications.js";
-import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.js";
-// core components/views for RTL layout
-import RTLPage from "views/RTLPage/RTLPage.js";
+//import NotificationsPage from "views/Notifications/Notifications.js";
 
 const dashboardRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin",
   },
   {
     path: "/user",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
+    name: "Customer List",
     icon: Person,
     component: UserProfile,
     layout: "/admin",
   },
   {
     path: "/table",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
+    name: "Bin List",
     icon: "content_paste",
     component: TableList,
     layout: "/admin",
   },
   {
     path: "/typography",
-    name: "Typography",
-    rtlName: "طباعة",
+    name: "Truck List",
     icon: LibraryBooks,
     component: Typography,
     layout: "/admin",
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin",
-  },
+  //{
+  // path: "/icons",
+  //  name: "Icons",
+  //  icon: BubbleChart,
+  //  component: Icons,
+  //  layout: "/admin",
+  //},
   {
     path: "/maps",
     name: "Maps",
-    rtlName: "خرائط",
     icon: LocationOn,
     component: Maps,
     layout: "/admin",
@@ -90,35 +79,17 @@ const dashboardRoutes = [
   {
     path: "/image",
     name: "Image",
-    rtlName: "الصورة",
     icon: ImageIcon,
     component: Image,
     layout: "/admin",
   },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin",
-  },
-  {
-    path: "/rtl-page",
-    name: "RTL Support",
-    rtlName: "پشتیبانی از راست به چپ",
-    icon: Language,
-    component: RTLPage,
-    layout: "/rtl",
-  },
-  {
-    path: "/upgrade-to-pro",
-    name: "Upgrade To PRO",
-    rtlName: "التطور للاحترافية",
-    icon: Unarchive,
-    component: UpgradeToPro,
-    layout: "/admin",
-  },
+  //{
+  //  path: "/notifications",
+  //  name: "Notifications",
+  //  icon: Notifications,
+  //  component: NotificationsPage,
+  //  layout: "/admin",
+  //},
 ];
 
 export default dashboardRoutes;
